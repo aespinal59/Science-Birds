34dd1bf327a3de82f338e622dafc7d1fb4168cf2
Matthew Laikhram <mlaikhram@gmail.com>
2019-05-07 00:43:26 -0400

    minor additions and prep for webgl build

---
+++ Assets/Scripts/OscillateScale.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OscillateScale : MonoBehaviour
+{
+    public float A;
+    public float b;
+
+    private Vector3 initScale;
+
+    void Awake()
+    {
+        initScale = transform.localScale;
+    }
+
+    void FixedUpdate()
+    {
+        float newScaleShift = A * Mathf.Sin(b * Time.time);
+        transform.localScale = new Vector3(initScale.x + newScaleShift, initScale.y + newScaleShift, initScale.z);
+    }
+}
